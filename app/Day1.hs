c8d69a9e56ce2e6d6be269a6108283111e025562
vikrem <vikrem@users.noreply.github.com>
2022-12-01 02:34:15 -0500

    day1

---
+++ app/Day1.hs
@@ -0,0 +1,29 @@
+module Day1 where
+import Text.Parsec.Text
+import Text.Parsec
+import Text.Read
+import Data.List (maximum)
+
+day1 :: IO ()
+day1 = do
+  input <- decodeUtf8 <$> readFileBS "input/day1.txt"
+  let parsed = runParser elfParser () "input" input
+  case parsed of 
+    Left err -> print err
+    Right xs -> do
+        print $ partOne xs
+        print $ partTwo xs
+
+
+partOne :: [[Int]] -> Int
+partOne = maximum . fmap sum
+
+partTwo :: [[Int]] -> Int
+partTwo = sum . take 3 . reverse . sort . fmap sum
+
+elfParser :: Parser [[Int]]
+elfParser = do
+    let elf = many1 digit `endBy` newline
+    let elves = elf `sepBy` newline
+    -- Parse characters to integers, throw out bad parses
+    (fmap . fmap) (mapMaybe readMaybe) elves
---
+++ app/Main.hs
@@ -0,0 +1,4 @@
+module Main where
+
+main :: IO ()
+main = putStrLn "Use a repl to run the code"
